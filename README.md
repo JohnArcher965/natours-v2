d9403611a1c552ceb7ea165faea8372fc19d316d
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-12 15:19:23 +0000

    Setting up mongoose

---
+++ README.md
@@ -0,0 +1,8 @@
+# Natours Application
+
+As part of the NodeJS course by Jonas Schmedtmann
+
+- Link to course: https://www.udemy.com/course/nodejs-express-mongodb-bootcamp
+- Links to Intructor: https://www.udemy.com/user/jonasschmedtmann/
+  - Twitter: https://twitter.com/jonasschmedtman
+  - Website: https://codingheroes.io/
---
+++ server.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection Successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
