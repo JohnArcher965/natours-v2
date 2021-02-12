83718e86215c20ad2d28765931e198894d902a31
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-12 16:17:29 +0000

    refactoring code for mvc

--- server.js
+++ server.js
@@ -19,22 +19,6 @@ mongoose
   })
   .then(() => console.log('DB Connection Successful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
