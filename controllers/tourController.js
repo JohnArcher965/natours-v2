53a07c64da3f3278b8ee8c9161505500d045cdf7
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-15 18:04:21 +0000

    Correcting Error in import of catchAsync

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,8 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
-const catchAsync = '../utils/catchAsync.js';
+
+const catchAsync = require('../utils/catchAsync.js');
+
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
