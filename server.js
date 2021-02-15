1fabcf2e30f45ec9f93f172f852fcf3bf6f9fba9
Johnathan Wilson <johnathan@kellerson.co.uk>
2021-02-15 21:52:21 +0000

    Handling unhandled rejections by shutting down the server

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ mongoose
   .then(() => console.log('DB Connection Successful'));
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
